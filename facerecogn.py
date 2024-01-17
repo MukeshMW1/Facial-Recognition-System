d06e79abb3d85bd6e07a7f650b9eff52ff5520c1
MukeshMW1 <142603749+MukeshMW1@users.noreply.github.com>
2024-01-17 08:41:38 +0545

    Update facerecogn.py

--- facerecogn.py
+++ facerecogn.py
@@ -53,48 +53,48 @@ faces = cv2.CascadeClassifier(cascade_path)
 knn=KNeighborsClassifier(n_neighbors=5)
 knn.fit(FACES,LABELS)
 
-# COLS_NAMES=['NAMES','TIME','DATE']
+COLS_NAMES=['NAMES','TIME','DATE']
     
 
-# while True:
-#     ret,frame=video.read()
-#     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+while True:
+    ret,frame=video.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     
-#     faces=faceDetect.detectMultiScale(gray, 1.3, 5)
-#     for(x,y,w,h) in faces:
-#         crop_img=frame[y:y+h,x:x+w,:]
-#         resize_img=cv2.resize(crop_img,(50,50)).flatten().reshape(1,-1)
-#         output= knn.predict(resize_img)
-#         ts=time.time()
-#         cv2.putText(frame,output[0], (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-#         date=datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
-#         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-#         timestamp=datetime.fromtimestamp(ts).strftime("%H-%M-%S")
-#         exist=os.path.isfile("Attendance/Attendance_"+ date + ".csv")
-#         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-#         attendance=[str(output[0]),str(timestamp),str(date)]
+    faces=faceDetect.detectMultiScale(gray, 1.3, 5)
+    for(x,y,w,h) in faces:
+        crop_img=frame[y:y+h,x:x+w,:]
+        resize_img=cv2.resize(crop_img,(50,50)).flatten().reshape(1,-1)
+        output= knn.predict(resize_img)
+        ts=time.time()
+        cv2.putText(frame,output[0], (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        date=datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        timestamp=datetime.fromtimestamp(ts).strftime("%H-%M-%S")
+        exist=os.path.isfile("Attendance/Attendance_"+ date + ".csv")
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        attendance=[str(output[0]),str(timestamp),str(date)]
         
-#     cv2.imshow("frame",frame)
+    cv2.imshow("frame",frame)
     
-#     k=cv2.waitKey(1)
-#     if k==ord('o'): 
-#         speak("Attendance Taken...")
-#         time.sleep(3)
-#         if exist:
-#               with open("Attendance/Attendance_"+ date + ".csv" , "a") as csvfile:
-#                writer= csv.writer(csvfile)
-#                writer.writerow(attendance) 
-#         else:
-#             with open("Attendance/Attendance_"+ date + ".csv" , "a") as csvfile:
-#                writer= csv.writer(csvfile)
-#                writer.writerow(COLS_NAMES)
-#                writer.writerow(attendance) 
-#             csvfile.close()                        
-#     if k==ord('q'):
-#       break
+    k=cv2.waitKey(1)
+    if k==ord('o'): 
+        speak("Attendance Taken...")
+        time.sleep(3)
+        if exist:
+              with open("Attendance/Attendance_"+ date + ".csv" , "a") as csvfile:
+               writer= csv.writer(csvfile)
+               writer.writerow(attendance) 
+        else:
+            with open("Attendance/Attendance_"+ date + ".csv" , "a") as csvfile:
+               writer= csv.writer(csvfile)
+               writer.writerow(COLS_NAMES)
+               writer.writerow(attendance) 
+            csvfile.close()                        
+    if k==ord('q'):
+      break
  
  
-# video.release()
-# cv2.destroyAllWindows() 
+video.release()
+cv2.destroyAllWindows() 
 
 
